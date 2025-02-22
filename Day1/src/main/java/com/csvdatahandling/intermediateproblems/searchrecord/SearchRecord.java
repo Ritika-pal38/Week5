adf129cebd3884875e547e425e00605d346cf92c
Ritika-pal38 <palritika294@gmail.com>
2025-02-22 11:43:14 +0530

    [Ritika] Added code to search record in a CSV file

---
+++ Day1/src/main/java/com/csvdatahandling/intermediateproblems/searchrecord/SearchRecord.java
@@ -0,0 +1,28 @@
+package com.csvdatahandling.intermediateproblems.searchrecord;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.*;
+
+
+public class SearchRecord {
+    public static void main(String[] args) {
+         String filename = "C:\\Users\\naman\\OneDrive\\Desktop\\Week-05---IO-Programming\\Day1\\src\\main\\java\\com\\csvdatahandling\\intermediateproblems\\searchrecord\\employees.csv";
+         String target = "Naman";
+         try(CSVReader reader = new CSVReader(new FileReader(filename))){
+             String [] column;
+             while ((column = reader.readNext())!= null){
+                 if(column[1].equals(target)){
+                     System.out.println("ID: "+column[0]+", Name: "+column[1]+", Department: "+column[2]+", Salary: "+column[3]);
+                 }
+             }
+         } catch (FileNotFoundException e) {
+             throw new RuntimeException(e);
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         } catch (CsvValidationException e) {
+             throw new RuntimeException(e);
+         }
+    }
+}
