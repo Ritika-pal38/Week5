7f8f3b89963a725c90c7a69e6e0e1943601cf35e
Ritika-pal38 <palritika294@gmail.com>
2025-02-22 11:38:01 +0530

    [Ritika] Added code for read and count rows in a CSV file

---
+++ Day1/src/main/java/com/csvdatahandling/basicproblems/readandcountrows/ReadAndCountRows.java
@@ -0,0 +1,28 @@
+package com.csvdatahandling.basicproblems.readandcountrows;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import javax.imageio.IIOException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadAndCountRows {
+    public static void main(String[] args)throws IOException {
+        String filename = "C:\\Users\\naman\\OneDrive\\Desktop\\Week-05---IO-Programming\\Day1\\src\\main\\java\\com\\csvdatahandling\\basicproblems\\readandcountrows\\students.csv";
+
+        try(CSVReader reader = new CSVReader(new FileReader(filename))){
+            String [] column;
+            int rows=0;
+            while((column = reader.readNext())!=null){
+                rows++;
+            }
+            System.out.println("Number of rows: "+rows);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
